0c7e4a934b81db74e86a5c20090c088123a15b94
alen <germanalen@gmail.com>
2018-04-01 19:19:56 +0600

    lobby.js load 2 game for quicker testing

--- src/main/webapp/lobby.js
+++ src/main/webapp/lobby.js
@@ -1,51 +1,56 @@
+var lobbyIndex = 0;
+var lobby1 = new Lobby(lobbyIndex, 'game1');
+var lobby2 = new Lobby(lobbyIndex, 'game2');
 
-var lobbyIndex = parseInt(sessionStorage.getItem('lobbyIndex'));
-document.title = 'Lobby' + lobbyIndex;
+function Lobby(lobbyIndex, gameParent) {
+    var socket = new WebSocket("ws://" + location.host + "/WebGame/websocketendpoint");
+    var game = null;
 
-var socket = new WebSocket("ws://" + location.host + "/WebGame/websocketendpoint");
-var game = null;
+    socket.onopen = function(event) {
+        console.log(gameParent + ' onopen::' + JSON.stringify(event, null, 4));
 
-socket.onopen = function(event) {
-    console.log('onopen::' + JSON.stringify(event, null, 4));
-    //var lobbyIndex = 0;
-    var playerId = getRandomInt(1, 2147483637); // almost upper limit of int32 signed
+        var playerId = getRandomInt(1, 2147483637); // almost upper limit of int32 signed
 
-    var servermsg = new Protocol.Server.ServerMsg();
-    servermsg.lobbyIndex = lobbyIndex;
-    servermsg.lobbyCmd = new Protocol.Server.LobbyCmd();
-    servermsg.lobbyCmd.addPlayerId = playerId;
+        var servermsg = new Protocol.Server.ServerMsg();
+        servermsg.lobbyIndex = lobbyIndex;
+        servermsg.lobbyCmd = new Protocol.Server.LobbyCmd();
+        servermsg.lobbyCmd.addPlayerId = playerId;
 
-    socket.send(servermsg.bytes());
-}
-
-socket.onmessage = function(event) {
-    var blob = event.data;
-    var reader = new FileReader();
-    reader.onload = function() {
-        var arrayBuffer = reader.result;
-        var clientMsg = Protocol.Client.ClientMsg.parse(new ByteReader(arrayBuffer));
-        lobbyOnMessage(clientMsg);
+        socket.send(servermsg.bytes());
     }
-    reader.readAsArrayBuffer(blob);
-}
 
-function lobbyOnMessage(clientMsg) {
-    if (clientMsg.startGame != null) {
-        document.getElementById('game').innerHTML = '';
-        game = CreateGame("game", socket, lobbyIndex);
-    } else if (clientMsg.gameMsg != null) {
-        game.onmessage(clientMsg.gameMsg);
+    socket.onclose = function(event) {
+        console.log(gameParent + ' onclose::' + JSON.stringify(event, null, 4));
+        // go back to main page
     }
+
+    socket.onerror = function(event) {
+        console.log(gameParent + ' onerror::' + JSON.stringify(event, null, 4));
+    }
+
+    socket.onmessage = function(event) {
+        var blob = event.data;
+        var reader = new FileReader();
+        reader.onload = function() {
+            var arrayBuffer = reader.result;
+            var clientMsg = Protocol.Client.ClientMsg.parse(new ByteReader(arrayBuffer));
+            lobbyOnMessage(clientMsg);
+        }
+        reader.readAsArrayBuffer(blob);
+    }
+
+    function lobbyOnMessage(clientMsg) {
+        if (clientMsg.startGame != null) {
+            document.getElementById(gameParent).innerHTML = '';
+            game = CreateGame(gameParent, socket, lobbyIndex);
+        } else if (clientMsg.gameMsg != null) {
+            game.onmessage(clientMsg.gameMsg);
+        }
+    }
+    this.socket = socket;
+    this.game = game;
 }
 
-socket.onclose = function(event) {
-    console.log('onclose::' + JSON.stringify(event, null, 4));
-    // go back to main page
-}
-
-socket.onerror = function(event) {
-    console.log('onerror::' + JSON.stringify(event, null, 4));
-}
 
 
 function toArrayBuf(int8arr) {
